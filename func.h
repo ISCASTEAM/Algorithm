04713061fd8b97e3fabaa768f8d3b56e0101b391
cuixuage <1157097502@qq.com>
2018-12-21 20:28:30 +0800

    add funciton header file

---
+++ func.h
@@ -0,0 +1,32 @@
+#ifndef __FUNC_H__
+#define __FUNC_H__
+
+#include<string.h>
+#include<stdio.h>
+#include<iostream>
+#include <fstream>
+#include<string>
+#include<set>
+#include<stack>
+#include<vector>
+#include<sstream>
+#include<queue>
+#include<limits>
+#include<map>
+#include<unordered_map>
+#include<hash_set>
+#include <utility>
+#include<algorithm>
+using std::cout;
+using std::endl;
+using std::string;
+using std::stack;
+using std::vector;
+using std::set;
+using std::multiset;
+using std::map;
+using std::stringstream;
+using std::unordered_map;
+using std::queue;
+using std::priority_queue;
+#endif // __FUNC__
